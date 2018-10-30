42483f9dcc5cf31d1f82c2fd8ad191a63c2697c4
swatturkar <swatturkar@g.harvard.edu>
2018-10-29 20:54:58 -0400

    initial

---
+++ src/main/java/com/suhas/openshiftdemo/OpenshiftDemoApplication.java
@@ -0,0 +1,12 @@
+package com.suhas.openshiftdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftDemoApplication.class, args);
+	}
+}
